acb44510e43a416a68b92606b8889f27c0c19244
vectorman1 <boris.lachevbg00@gmail.com>
2015-11-25 23:04:13 +0200

    added status and stuff

--- app/src/main/java/tk/lachev/evestats/activities/ServerStatus.java
+++ app/src/main/java/tk/lachev/evestats/activities/ServerStatus.java
@@ -1,13 +1,19 @@
 package tk.lachev.evestats.activities;
 
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.tlabs.eve.EveNetwork;
@@ -21,31 +27,55 @@ import java.net.URL;
 
 import tk.lachev.evestats.R;
 
+import static tk.lachev.evestats.R.*;
+
 public class ServerStatus extends AppCompatActivity {
 
+
+    public TextView text1;
+    public TextView text2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_server_status);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(layout.activity_server_status);
+        Toolbar toolbar = (Toolbar) findViewById(id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        text1 = (TextView) findViewById(R.id.server_status_textview);
+        text2 = (TextView) findViewById(id.players_online_textview);
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Refreshing...", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                serverStatusGet.execute();
             }
+
         });
+
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        ServerStatusGet serverStatusGet = new ServerStatusGet();
+
+
         Log.d(ServerStatus.class.toString(), "Executing");
         serverStatusGet.execute();
     }
 
 }
 class ServerStatusGet extends AsyncTask<EveNetwork, ServerStatusRequest, ServerStatusResponse> {
+
+    private final TextView _playersOnline;
+    private final TextView _statusText;
+
+
+    public ServerStatusGet(TextView statusText, TextView playersOnline) {
+        this._statusText = statusText;
+        this._playersOnline = playersOnline;
+    }
 
     @Override
     protected ServerStatusResponse doInBackground(EveNetwork... params) {
@@ -59,13 +89,16 @@ class ServerStatusGet extends AsyncTask<EveNetwork, ServerStatusRequest, ServerS
     @Override
     protected void onPostExecute(ServerStatusResponse status) {
         Log.d("App", "onPostExecute");
-        TextView tv = null;
-        tv = (TextView) tv.findViewById(R.id.server_status_textview);
         if(status.getServerOpen()) {
-            tv.setText("Online");
+            Log.d("APP", "SERVER IS OPEN, CHANGING TEXT");
+            _statusText.setText("Online!");
+            _statusText.setTextColor(ColorStateList.valueOf(Color.GREEN));
+            _playersOnline.setText(status.getOnlinePlayers() + " players online currently on Tranquility!");
         }
         else {
-            tv.setText("Offline");
+            _statusText.setText("Offline!");
+            _statusText.setTextColor(ColorStateList.valueOf(Color.RED));
         }
+
     }
 }
